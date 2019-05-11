5ff90dda44b838e6cd348bb5c822bc5f28ca7d4c
Ryan Saltamacchio <ryanasalt@gmail.com>
2019-05-11 01:49:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  array = array.unshift(element);
   return array;
 }
 
